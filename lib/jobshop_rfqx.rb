9b2848794fc25e43ece759fb5ea9df4a8d7e33b7
jc <emclab2011@gmail.com>
2014-02-20 19:41:11 -0600

    JC: added @view_in_config and @max_pagination to application controller. spec passes.

--- lib/jobshop_rfqx.rb
+++ lib/jobshop_rfqx.rb
@@ -1,7 +1,7 @@
 require "jobshop_rfqx/engine"
 
 module JobshopRfqx
-  mattr_accessor :customer_class, :quote_index_path, :order_index_path, :mfg_process_index_path
+  mattr_accessor :customer_class, :quote_index_path, :order_index_path, :mfg_process_index_path, :quote_task_index_path
   
   def self.customer_class
     @@customer_class.constantize
