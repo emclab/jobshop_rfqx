2e52c3acfa8f167c795f552fdaa9db34d23e62fc
jc <emclab2011@gmail.com>
2014-03-10 21:33:50 -0500

    JC: fixed bug in customer name autocomplete by adding the js function to the rfq.js.

--- app/assets/javascripts/jobshop_rfqx/rfqs.js
+++ app/assets/javascripts/jobshop_rfqx/rfqs.js
@@ -4,3 +4,15 @@ $(function() {
 	$( "#rfq_est_production_date" ).datepicker({dateFormat: 'yy-mm-dd'});
 	
 });
+
+//for customer field autocomplete on new rfq view.
+$(function() {
+    return $('#rfq_customer_name_autocomplete').autocomplete({
+        minLength: 1,
+        source: $('#rfq_customer_name_autocomplete').data('autocomplete-source'),  //'#..' can NOT be replaced with this
+        select: function(event, ui) {
+            //alert('fired!');
+            $('#rfq_customer_name_autocomplete').val(ui.item.value);
+        },
+    });
+});
