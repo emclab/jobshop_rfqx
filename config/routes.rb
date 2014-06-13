ffd529c6774b34007276455cf3314a96afbfc83b
jc <emclab2011@gmail.com>
2014-06-13 16:32:40 -0500

    JC: changed from put to get in routes for search/stat results.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ JobshopRfqx::Engine.routes.draw do
   resources :rfqs do
     collection do
       get :search
-      put :search_results
+      get :search_results
       get :stats
-      put :stats_results
+      get :stats_results
       get :copy_last
     end
   end
